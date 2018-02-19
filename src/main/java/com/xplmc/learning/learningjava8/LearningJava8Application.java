11edb94e6bba6218d176c26b8de8c3bdbf4e0ab4
lurkerping <lurkerping@gmail.com>
2018-02-19 21:12:52 +0800

    initial commit

---
+++ src/main/java/com/xplmc/learning/learningjava8/LearningJava8Application.java
@@ -0,0 +1,12 @@
+package com.xplmc.learning.learningjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJava8Application.class, args);
+	}
+}
