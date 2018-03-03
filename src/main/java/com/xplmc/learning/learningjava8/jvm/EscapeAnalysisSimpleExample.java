cdfade9a3da844e42b25a1760d1a8e31b66efa63
xiaoping <lurkerping@gmail.com>
2018-03-03 21:27:10 +0800

    Escaping Analysis simple example

---
+++ src/main/java/com/xplmc/learning/learningjava8/jvm/EscapeAnalysisSimpleExample.java
@@ -0,0 +1,32 @@
+package com.xplmc.learning.learningjava8.jvm;
+
+import com.xplmc.learning.learningjava8.methodhandle.GirlFriend;
+
+/**
+ * escaping analysis, start from java6u20
+ * 1/detecting an object that is only used in the method
+ * 2/avoiding heap memory allocating
+ */
+public class EscapeAnalysisSimpleExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        long count = Integer.MAX_VALUE * 100L;
+        EscapeAnalysisSimpleExample e = new EscapeAnalysisSimpleExample();
+        for (long i = 0; i < count; i++) {
+            e.newObject();
+        }
+    }
+
+    /**
+     * GirlFriend match the escaping condition
+     */
+    private void newObject() {
+        GirlFriend girlFriend = new GirlFriend();
+        girlFriend.setBody("nice");
+        girlFriend.setFace("pretty");
+        girlFriend.setName("Lyanna");
+        girlFriend.setSex("female");
+        girlFriend.setNature("good");
+    }
+
+}
