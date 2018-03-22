a1bb98f91b2173fd9b1e201d3ab4f2ade9f9aaf4
lurkerping <lurkerping@gmail.com>
2018-03-22 20:50:17 +0800

    add cloassloader example

---
+++ src/main/java/com/xplmc/learning/learningjava8/classloading/ClassLoadingExample.java
@@ -0,0 +1,28 @@
+package com.xplmc.learning.learningjava8.classloading;
+
+import com.sun.nio.zipfs.ZipInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+
+/**
+ * class loading parent-delegation model example
+ */
+public class ClassLoadingExample {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClassLoadingExample.class);
+
+    public static void main(String[] args) {
+        //Bootstrap ClassLoader
+        logger.info("BigDecimal's classloader: {}", BigDecimal.class.getClassLoader());
+        logger.info("Integer's classloader: {}", Integer.class.getClassLoader());
+
+        //Extension ClassLoader
+        logger.info("ZipInfo's classloader: {}", ZipInfo.class.getClassLoader());
+
+        //App ClassLoader
+        logger.info("ClassLoadingExample's classloader: {}", ClassLoadingExample.class.getClassLoader());
+    }
+
+}
